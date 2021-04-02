eb6d87ebb38201b4446934e54d77290f3f9a587f
it小婷 <1677810050@qq.com>
2021-04-02 16:05:58 +0800

    #include<stdio.h> int change(int x) {  if((x % 400 == 0 )||( x % 4 == 0 && x % 100 != 0))  {   return 1;  }  else  {   return 0;  } } int main() {  int t;  int year;  scanf_s("%d", &year);  t=change(year);  if (t == 1)  {   printf("是闰年\n");  }  else   printf("不是闰年\n");  return 0; }

--- Project17/Project17/源.cpp
+++ Project17/Project17/源.cpp
@@ -1,27 +1,26 @@
 #include<stdio.h>
-#include<math.h>
-int pan(int x)
+int change(int x)
 {
-	int y;
-	for (y = 2; y <= (int)sqrt(x); y += 2)
+	if((x % 400 == 0 )||( x % 4 == 0 && x % 100 != 0))
 	{
-		if (x % y == 0)
-		{
-			return 0;
-		}
-	}
-	if (y > (int)sqrt(x))
 		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
 int main()
 {
-	int i = 0;
-	int t = 0;
-	scanf_s("%d", &i);
-	t = pan(i);
+	int t;
+	int year;
+	scanf_s("%d", &year);
+	t=change(year);
 	if (t == 1)
-		printf("%d是素数\n", i);
+	{
+		printf("是闰年\n");
+	}
 	else
-		printf("%d不是素数\n", i);
+		printf("不是闰年\n");
 	return 0;
 }

904ef95fd5dd78a9af215d9db81c2cd8d73ca56e
it小婷 <1677810050@qq.com>
2021-04-03 15:05:22 +0800

    #include<stdio.h> void game(int* x) {  (*x)++; } int main() {  int num = 0;  //int t;  while(1)  {   game(&num);   printf("%d\n", num);  }  return 0; }

--- Project18/Project18/源.cpp
+++ Project18/Project18/源.cpp
@@ -1,41 +1,16 @@
 #include<stdio.h>
-#include<string.h>
-int seek(int x, int y, int z, int arr[])
+void game(int* x)
 {
-	int i = 0;
-	int k;
-	scanf_s("%d", &k);
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	while (x <= y)
-	{
-		if (arr[z] > k)
-		{
-			y = z - 1;
-			z = (y + x) / 2;
-		}
-		else if (arr[z] < k)
-		{
-			x = z + 1;
-			z = (y + x) / 2;
-		}
-		else
-			return 0;
-	}
-	return 1;
+	(*x)++;
 }
 int main()
 {
-	int t = 0;
-	int arr[] = { 1,2,3,4,5,6,7,8,9 };
-	int left = 0;
-	int right = sizeof(arr) / sizeof(arr[0]);
-	int mid = (left + right) / 2;  arr[mid] =
-		t = seek(left, right, mid, arr);
-	if (t == 0)
+	int num = 0;
+	//int t;
+	while(1)
 	{
-		printf("找到了\n");
+		game(&num);
+		printf("%d\n", num);
 	}
-	else
-		printf("找不到\n");
 	return 0;
 }

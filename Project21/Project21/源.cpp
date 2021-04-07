304aea86898b4e6023ab9bba4f2605037ccd2a39
it小婷 <1677810050@qq.com>
2021-04-07 19:35:08 +0800

    #include<stdio.h> int main() {  int arr[10];  int i;  int sum = 0;  for (i = 0; i < 10; i++)  {   scanf_s("%d", &arr[i]);  }  for (i = 9; i>= 0; i--)  {   printf("%d\t", arr[i]);   sum += i;  }  printf("%d\n", sum);  return 0; }

---
+++ Project21/Project21/源.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int arr[10];
+	int i;
+	int sum = 0;
+	for (i = 0; i < 10; i++)
+	{
+		scanf_s("%d", &arr[i]);
+	}
+	for (i = 9; i>= 0; i--)
+	{
+		printf("%d\t", arr[i]);
+		sum += i;
+	}
+	printf("%d\n", sum);
+	return 0;
+}

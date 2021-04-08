f1a0c27e87a669a4f5cb1d88a0465a27921a1bf4
it小婷 <1677810050@qq.com>
2021-04-08 22:22:33 +0800

    #include<stdio.h> int main() {  int i;  int arr[10];  for (i = 0; i < 10; i++)  {   scanf_s("%d", &arr[i]);  }  for (i = 9; i >= 0; i--)  {   printf("%d\t", arr[i]);  }  return 0; }

--- Project21/Project21/源.cpp
+++ Project21/Project21/源.cpp
@@ -1,27 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int sum = 0;
-	float ave = 1.0;
 	int i;
-	int k[5];
-	float t = sizeof(k) / sizeof(k[0]);
-	for (i = 0; i < 5; i++)
+	int arr[10];
+	for (i = 0; i < 10; i++)
 	{
-		scanf_s("%d", &k[i]);
+		scanf_s("%d", &arr[i]);
 	}
-	for (i = 0; i < 5; i++)
+	for (i = 9; i >= 0; i--)
 	{
-		sum += k[i];
+		printf("%d\t", arr[i]);
 	}
-	ave = sum / t;
-	for (i = 0; i < 5; i++)
-	{
-		if (k[i] > ave)
-		{
-			printf("%d\n", k[i]);
-		}
-	}
-	printf("%.3f\n", ave);
 	return 0;
 }
